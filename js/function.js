625f9a29434a97505538a7543eda012f83457b6a
SabbirMollah <mollahsabbir@gmail.com>
2018-09-30 22:05:22 +0600

    First Commit

---
+++ js/function.js
@@ -0,0 +1,211 @@
+
+/* ----------------------- 
+    GLOBALS 
+-------------------------*/
+var sections = [
+		{	sentence: "This is Sabbir Mollah",
+		},
+		{	sentence: "I am a Rubber Duck",
+		},
+		{	sentence: "I love Books",
+		},
+		{	sentence: "Error C://life.run, no contexts for life were found",
+		},
+		{	sentence: "I write spells for computers",
+		},
+		{	sentence: "I have 'borrowed' this webpage design",
+		},
+		{	sentence: "I am a proud Parseltongue",
+		},
+		{	sentence: "I belong to house <b>slytherin<b>",
+		},
+		{	sentence: "1f u c4n r34d th1s u 1t r34lly m34ns n0th1ng",
+		},
+		{	sentence: "I never sleep on my Keybs43ufbo3wwyf8hdns839",
+		},
+		{	sentence: "42 is the answer of Life",
+		},
+		
+	];
+var i = 0;
+var j = 0;
+var k = 0;
+var lengthSentence = 0;
+var lengthArray = sections.length;
+var forward = true;
+var beginning = ">>>";
+var currentPart = "";
+var interval = 50;
+var opening = false;
+
+
+/* ----------------------- 
+    TYPING 
+-------------------------*/
+function writing(text){
+	lengthSentence = sections[i].sentence.length;
+	var body = $("body");
+	if(!opening){ // first part
+		setTimeout(function(){	
+			if(k < beginning.length){
+				if(beginning[k] === "<"){
+					currentPart += ' <br id="brName">';
+					k=k+4;
+				}
+				currentPart += beginning[k];
+				text.html(currentPart);
+				k++;
+				writing(text);			
+			}else if(k === (beginning.length)){
+				currentPart += " Hello World !! <br> <br> "; //write anything to add in front
+				text.html(currentPart);
+				opening = true;
+				writing(text);
+			}
+		},interval);
+	}else if(opening){ // sentences
+		setTimeout(function(){
+			interval = 50;
+			if(j === lengthSentence){
+				forward = false;
+			}
+			if(j === lengthSentence-2){
+				$(".afterTyping").one().addClass("onScreen");
+			}
+			if( j === lengthSentence-1 && forward){
+				interval = 2000;
+			}
+			if(j < lengthSentence && forward ){
+				if(sections[i].sentence[j] === "&"){
+					currentPart += "<strong>";
+				}else if(sections[i].sentence[j] === "%"){
+					currentPart += "</strong>";
+				}
+				else{
+					currentPart += sections[i].sentence[j];
+				}
+				text.html(currentPart);
+				j++;
+			}else if(j > 0 && !forward){
+				if(sections[i].sentence[j] === "&"){
+					currentPart = currentPart.slice(0, - 8);
+				}else if(sections[i].sentence[j] === "%"){
+					currentPart = currentPart.slice(0, - 9);
+				}
+				else{
+					currentPart = currentPart.slice(0, - 1); 
+				}
+				text.html(currentPart);
+				j--;
+			}else if(j === 0){
+				forward = true;
+				/*body.css({
+					"background" : sections[i].background});*/
+				i++; // loop fra sezioni
+			}
+			if(i === lengthArray){
+				i = 0;
+			}
+			writing(text);
+		}, interval);
+	}
+}
+
+
+/* ----------------------- 
+    BACKGROUND Color loop function 
+-------------------------*/
+function changebackground(){
+	var body = $("body");
+    body.css({
+    	"background" : "rgb(20, 20, 20)"
+    });
+	$(".coloredHover").css({
+    	"color" : "rgb(47, 54, 64)"
+    });
+
+}
+
+
+
+	
+$(document).ready(function(){
+	/*--------------------
+		BACKGROUND STUFF
+	----------------------*/
+	changebackground();
+	setTimeout(function(){
+		$("body").removeClass("noTransition");
+		$("fixedBg").removeClass("noTransition");
+		changebackground();
+	}, 2000)   
+	setInterval(function(){
+		changebackground();
+	}, 20000);
+
+
+	/*--------------------
+		TYPING 
+	----------------------*/
+	var firstTimer = 3000;
+	var text = $(".jstext");
+	setTimeout(function(){
+		writing(text);
+		//incipit(text);
+	}, firstTimer);
+	/*setTimeout(function(){
+	}, secondTimer);*/
+
+
+	/*--------------------
+		HOVER 
+	----------------------*/
+	if( navigator.userAgent.toLowerCase().indexOf('firefox') > -1 ){
+    	$("body").addClass("firefoxFix");
+	}
+});
+
+
+/*--------------------
+		HAVING FUN WITH TWEENMAX 
+----------------------*/
+$(document).ready(function($) {
+	var bgFixed = $(".fixedBg");
+	var elements = $(".fixedBg span");
+	var triggerHover = $(".loopCol");
+	tlHoverIn = new TimelineMax();
+	tlHoverOut = new TimelineMax();
+
+
+	triggerHover.hover(
+		function() {
+			tlHoverIn
+				.to($(this).next(".fixedBg"), 0.5, {autoAlpha: 1})
+				.to($(this).next(".fixedBg").find("span:nth-of-type(1)"), 0.8, { y: 0, ease: Expo.easeOut}, '0')
+				.to($(this).next(".fixedBg").find("span:nth-of-type(2)"), 0.8, { y: 0, ease: Expo.easeOut}, '0.02')
+				.to($(this).next(".fixedBg").find("span:nth-of-type(3)"), 0.8, { y: 0, ease: Expo.easeOut}, '0.04')
+				.to($(this).next(".fixedBg").find("span:nth-of-type(4)"), 0.8, { y: 0, ease: Expo.easeOut}, '0.06');
+		},
+		 function() {
+		 	tlHoverOut
+				.to($(this).next(".fixedBg"), 0.5, {autoAlpha: 0})
+				.to($(this).next(".fixedBg").find("span:nth-of-type(1)"), 0.8, { y: 20, ease: Expo.easeOut}, '0')
+				.to($(this).next(".fixedBg").find("span:nth-of-type(2)"), 0.8, { y: 40, ease: Expo.easeOut}, '0.02')
+				.to($(this).next(".fixedBg").find("span:nth-of-type(3)"), 0.8, { y: 60, ease: Expo.easeOut}, '0.04')
+				.to($(this).next(".fixedBg").find("span:nth-of-type(4)"), 0.8, { y: 80, ease: Expo.easeOut}, '0.06');
+		}
+	);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
